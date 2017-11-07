7a5ea3e7af6983b4bda0cc797493be6df9971ec8
John Tuttle <thejwt@gmail.com>
2017-11-07 22:55:31 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -26,31 +26,8 @@ class LandmarksController < ApplicationController
   end
 
   patch '/landmarks/:id' do
-    figure_1 = Figure.find_by(id: params[:id])
-    figure_1.update(name: params[:figure][:name])
-    if params[:figure][:landmark_ids] != nil
-      landmarks = []
-      params[:figure][:landmark_ids].each do |landmark|
-        landmarks << Landmark.find_by(name: landmark)
-      end
-      figure_1.update(landmarks: landmarks)
-    end
-    if params[:figure][:title_ids] != nil
-      titles = []
-      params[:figure][:title_ids].each do |title|
-        titles << Title.find_by(name: title)
-      end
-      figure_1.update(titles: titles)
-    end
-    if params[:landmark][:name] != nil
-      landmark = Landmark.new(name: params[:landmark][:name])
-      figure_1.landmarks << landmark
-    end
-    if params[:title][:name] != nil
-      title = Title.new(name: params[:title][:name])
-      figure_1.titles << title
-    end
-    figure_1.save
+    landmark = Landmark.find_by(id: params[:id])
+    landmark.update(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
     redirect to "/landmarks/#{landmark.id}"
   end
 
