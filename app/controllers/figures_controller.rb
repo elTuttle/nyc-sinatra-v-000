d42d02ba8a3e74ab5bc5fae8b908a8973837f903
John Tuttle <thejwt@gmail.com>
2017-11-07 20:43:47 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,10 +1,7 @@
 class FiguresController < Sinatra::Base
 
-  set :views, Proc.new { File.join(root, "../views/figures/") }
-  register Sinatra::Twitter::Bootstrap::Assets
-
   get '/figures/new' do
-    erb :new
+    erb :'/figures/new'
   end
 
 end
