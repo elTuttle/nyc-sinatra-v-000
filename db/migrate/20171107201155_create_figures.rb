e94c2f313ffb2a74029bee0628c7b73b9e7f8ffd
John Tuttle <thejwt@gmail.com>
2017-11-07 20:13:11 +0000

    automatically backed up by learn

--- db/migrate/20171107201155_create_figures.rb
+++ db/migrate/20171107201155_create_figures.rb
@@ -1,9 +1,7 @@
 class CreateFigures < ActiveRecord::Migration
   def change
-    create_table :landmarks do |t|
+    create_table :figures do |t|
       t.string :name
-      t.integer :figure_id
-      t.integer :year_completed
     end
   end
 end
