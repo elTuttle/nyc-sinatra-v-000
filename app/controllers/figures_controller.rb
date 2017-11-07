d464e0863ad062cd566872eb6142f55600c6c576
John Tuttle <thejwt@gmail.com>
2017-11-07 21:37:23 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -49,4 +49,31 @@ class FiguresController < Sinatra::Base
     erb :edit_figure
   end
 
+  patch '/figures/:id' do
+    figure_1 = Figure.find_by(name: params[:figure][:name])
+    if params[:figure][:landmark_ids] != nil
+      landmarks = []
+      params[:figure][:landmark_ids].each do |landmark|
+        landmarks << Landmark.find_by(name: landmark)
+      end
+      figure_1.update(landmarks: landmarks)
+    end
+    if params[:figure][:title_ids] != nil
+      titles = []
+      params[:figure][:title_ids].each do |title|
+        titles << Title.find_by(name: title)
+      end
+      figure_1.update(titles: titles)
+    end
+    if params[:landmark][:name] != nil
+      landmark = Landmark.new(name: params[:landmark][:name])
+      figure_1.landmarks << landmark
+    end
+    if params[:title][:name] != nil
+      title = Title.new(name: params[:title][:name])
+      figure_1.titles << title
+    end
+    figure_1.save
+  end
+
 end
